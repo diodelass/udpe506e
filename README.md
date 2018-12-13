72112c4936f4f56aac37752744e3da94de4a1357
Ellie D <sporillium@gmail.com>
2018-12-12 18:02:30 -0600

    added ipv6 support

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # udpe506e  
 
-- sends one UDP packet containing 508 `e`s to every address in the IPv4 address space.
+### say what?
+- sends one UDP packet containing 508 `e`s to every address on the internet.
 - eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 - uses port 101 (ASCII ordinal of `e`)
 - I recommend you don't run it
 - AGPL
 - R.I.P [e30e/e98e](https://linuxwit.ch/blog/2018/12/e98e/)
 
+### how do I build it?
+please don't.
+(use rustc)
--- udpe506e.rs
+++ udpe506e.rs
@@ -1,16 +1,41 @@
 use std::net::UdpSocket;
 
 fn main() {
-	let socket = UdpSocket::bind("0.0.0.0:0").expect("failed to bind to local address");
+	// send to everyone in the ipv4 address range. 
+	// takes an absolute minimum of about 6 hours with a gigabit connection and an arbitrarily fast computer.
+	let socket = UdpSocket::bind("[::]:0").expect("failed to bind to local address");
 	for w in 0..256 {
-		println!("{}.x.x.x",w);
-		for x in 0..256 {
-			println!("{}.{}.x.x",w,x);
-			for y in 0..256 {
-				for z in 0..256 {
-					let _ = socket.send_to(&[b'e';508],&format!("{}.{}.{}.{}:101",w,x,y,z));
-				}
-			}
-		}
+	for x in 0..256 {
+	for y in 0..256 {
+	for z in 0..256 {
+		let _ = socket.send_to(&[b'e';508],&format!("{}.{}.{}.{}:101",w,x,y,z));
 	}
+	}
+	}
+	}
+	
+	println!("finished ipv4. beginning ipv6...");
+
+	// send to everyone in the ipv6 address range.
+	// takes a minimum of about 10 septillion years with a gigabit connection and an arbitrarily fast computer.
+	// it is recommended to ensure that your computer's power supply has sufficient endurance for this operation.
+	for a in 0..65536 {
+	for b in 0..65536 {
+	for c in 0..65536 {
+	for d in 0..65536 {
+	for e in 0..65536 {
+	for f in 0..65536 {
+	for g in 0..65536 {
+	for h in 0..65536 {
+		let _ = socket.send_to(&[b'e';508],&format!("[{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}]:101",a,b,c,d,e,f,g,h));
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+
+	println!("done.");
 }
